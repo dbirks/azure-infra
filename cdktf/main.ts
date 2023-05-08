9aa08a50a2da7d4d146d133a9081cb8c05fc8b41
David Birks <david@birks.dev>
2023-05-08 19:31:22 -0400

    First try at deploying

--- cdktf/main.ts
+++ cdktf/main.ts
@@ -1,11 +1,33 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { kubernetesCluster } from "@cdktf/provider-azurerm";
+import { AzurermProvider } from "@cdktf/provider-azurerm/lib/provider";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // define resources here
+    const resourceGroupName = "personal";
+
+    new AzurermProvider(this, "azurerm", { features: {} });
+
+    new kubernetesCluster.KubernetesCluster(this, "k8s", {
+      name: "cdktf-k8s-cluster",
+      location: "eastus",
+      resourceGroupName,
+      defaultNodePool: {
+        name: "default",
+        nodeCount: 1,
+        vmSize: "Standard_B2s",
+
+        // osDiskSizeGb: 30,
+      },
+      dnsPrefix: "test",
+      identity: {
+        type: "SystemAssigned",
+      },
+      // kubernetesVersion: "1.26.3",
+    });
   }
 }
 
