89741719fff9f902010cdfd5f3c7a87cd69b6471
David Birks <david@birks.dev>
2023-05-11 16:16:48 -0400

    More work in progress, trying to get the flux setup to work smoothly

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # personal-aks-cluster
 
+## Overview
+
+<!-- This repo sets up a Kubernetes cluster on Azure, using  -->
+
+This repo sets up a Kubernetes cluster on Azure, using CDK for Terraform (cdktf) as the infrastructure-as-code tool.
+
+Repo overview:
+
+- `cdktf/`: The folder for CDK for Terraform, written with TypeScript
+  - `KubernetesCluster`: Creates the Kubernetes cluster itself, with a default node pool.
+  - `KubernetesClusterNodePool`: Creates an additional node pool with spot instances.
+  - `KubernetesClusterExtension`: Configures the Flux extension, which gives us the ability to do GitOps, controlling the yaml that will be applied to Kubernetes in a git repo (this repo).
+- `flux/`: 
+
+
 ```
 curl -LO https://app.getambassador.io/yaml/emissary/3.6.0/emissary-crds.yaml
 ```
--- cdktf/main.ts
+++ cdktf/main.ts
@@ -2,6 +2,8 @@ import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
 import { kubernetesCluster } from "@cdktf/provider-azurerm";
 import { AzurermProvider } from "@cdktf/provider-azurerm/lib/provider";
+import { KubernetesClusterNodePool } from "@cdktf/provider-azurerm/lib/kubernetes-cluster-node-pool";
+import { KubernetesClusterExtension } from "@cdktf/provider-azurerm/lib/kubernetes-cluster-extension";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
@@ -11,22 +13,59 @@ class MyStack extends TerraformStack {
 
     new AzurermProvider(this, "azurerm", { features: {} });
 
-    new kubernetesCluster.KubernetesCluster(this, "k8s", {
-      name: "cdktf-k8s-cluster",
+    const k8sCluster = new kubernetesCluster.KubernetesCluster(this, "k8s", {
+      name: "home",
+      dnsPrefix: "home", // prefix of the hostname of the publicly exposed cluster api
       location: "eastus",
       resourceGroupName,
+      kubernetesVersion: "1.26",
+      automaticChannelUpgrade: "patch",
+      identity: {
+        type: "SystemAssigned",
+      },
+
       defaultNodePool: {
         name: "default",
         nodeCount: 1,
         vmSize: "Standard_B2s",
+      },
+    });
 
-        // osDiskSizeGb: 30,
+    new KubernetesClusterNodePool(this, "spot-node-pool", {
+      name: "spot",
+      kubernetesClusterId: k8sCluster.id,
+      // vmSize: "Standard_B2s",
+      vmSize: "Standard_D2s_v3",
+      nodeCount: 0,
+      minCount: 0,
+      maxCount: 3,
+      priority: "Spot",
+      evictionPolicy: "Delete",
+      enableAutoScaling: true,
+      nodeLabels: {
+        "kubernetes.azure.com/scalesetpriority": "spot",
       },
-      dnsPrefix: "test",
-      identity: {
-        type: "SystemAssigned",
+      nodeTaints: ["kubernetes.azure.com/scalesetpriority=spot:NoSchedule"],
+    });
+
+    new KubernetesClusterExtension(this, "k8s-extension-flux", {
+      name: "flux",
+      clusterId: k8sCluster.id,
+      extensionType: "Microsoft.Flux",
+      releaseNamespace: "flux-system",
+      configurationSettings: {
+        "gitRepository.url": "https://github.com/dbirks/personal-aks-cluster",
+        "gitRepository.repositoryRef.branch": "main",
+        "sourceKind": "GitRepository",
+
+
+        // gitBranch: "main",
+        // gitPath: "flux",
+        // operatorInstanceName: "flux",
+        // operatorNamespace: "flux-system",
+        // syncInterval: "5m",
+        // deleteCRD: "true",
       },
-      // kubernetesVersion: "1.26.3",
     });
   }
 }
