4add930fd421a3f42e8cf35050f555c7a4a5ec41
David Birks <david@birks.dev>
2023-05-18 22:44:24 -0400

    wip private changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# personal-aks-cluster
+# azure-infra
 
 ## Overview
 
--- cdktf/main.ts
+++ cdktf/main.ts
@@ -2,20 +2,36 @@ import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
 import { kubernetesCluster } from "@cdktf/provider-azurerm";
 import { AzurermProvider } from "@cdktf/provider-azurerm/lib/provider";
-import { KubernetesClusterNodePool } from "@cdktf/provider-azurerm/lib/kubernetes-cluster-node-pool";
 import { VirtualNetwork } from "@cdktf/provider-azurerm/lib/virtual-network";
+import { PrivateDnsZone } from "@cdktf/provider-azurerm/lib/private-dns-zone";
+import { PrivateDnsResolver } from "@cdktf/provider-azurerm/lib/private-dns-resolver";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
     const resourceGroupName = "personal";
+    const location = "eastus";
 
     new AzurermProvider(this, "azurerm", { features: {} });
 
-    new VirtualNetwork(this, "vnet", {
+    // const wan = new VirtualWan(this, "wan", {
+    //   name: "birks-wan",
+    //   resourceGroupName,
+    //   location,
+    // });
+
+    // new VirtualHub(this, "hub", {
+    //   name: "birks-hub",
+    //   resourceGroupName,
+    //   location,
+    //   virtualWanId: wan.id,
+    //   addressPrefix: "10.10.0.0/20",
+    // });
+
+    const vnet = new VirtualNetwork(this, "vnet", {
       name: "personal",
-      location: "eastus",
+      location,
       resourceGroupName,
       addressSpace: ["10.10.0.0/20"],
       subnet: [
@@ -23,12 +39,74 @@ class MyStack extends TerraformStack {
           name: "default",
           addressPrefix: "10.10.0.0/23",
         },
+        {
+          name: "GatewaySubnet",
+          addressPrefix: "10.10.2.0/24",
+        },
+        {
+          name: "dns-resolvers",
+          addressPrefix: "10.10.3.0/28",
+        },
       ],
+      // gatewaySubnet: [
+      //   {
+      //     name: "gateway",
+      //     addressPrefix: "10.10.2.0/24",
+      //   }
     });
 
-    const k8sCluster = new kubernetesCluster.KubernetesCluster(this, "k8s", {
+    // new VpnGateway(this, "vpn", {
+    //   name: "Home connection",
+    //   resourceGroupName,
+    //   location,
+    //   virtualHubId: hub.id,
+    // });
+
+    // TODO: Re-enable this when my subscription can create spot instances
+    // new KubernetesClusterNodePool(this, "spot-node-pool", {
+    //   name: "spot",
+    //   kubernetesClusterId: k8sCluster.id,
+    //   vmSize: "Standard_B2s",
+    //   nodeCount: 1,
+    //   minCount: 1,
+    //   maxCount: 3,
+    //   priority: "Spot",
+    //   evictionPolicy: "Delete",
+    //   enableAutoScaling: true,
+    //   nodeLabels: {
+    //     "kubernetes.azure.com/scalesetpriority": "spot",
+    //   },
+    //   nodeTaints: ["kubernetes.azure.com/scalesetpriority=spot:NoSchedule"],
+    // });
+
+    // TODO: Configure flux automatically here when this PR is merged:
+    // https://github.com/hashicorp/terraform-provider-azurerm/issues/15011
+
+    new PrivateDnsZone(this, "private-dns", {
+      name: "birks.local",
+      resourceGroupName,
+    });
+
+    new PrivateDnsResolver(this, "private-dns-resolver", {
+      name: "private-resolver",
+      resourceGroupName,
+      location,
+      virtualNetworkId: vnet.id,
+      // privateDnsZoneConfigurations: [
+      //   {
+      //     name: "default",
+      //     privateDnsZoneId: "birks.local",
+      //     registrationEnabled: true,
+      //     resolutionEnabled: true,
+      //   },
+      // ],
+    });
+
+    new kubernetesCluster.KubernetesCluster(this, "k8s", {
       name: "home",
       dnsPrefix: "home", // prefix of the hostname of the publicly exposed cluster api
+      privateClusterEnabled: true,
+      // privateDnsZoneId: privateDns.id,
       location: "eastus",
       resourceGroupName,
       kubernetesVersion: "1.26",
@@ -42,25 +120,6 @@ class MyStack extends TerraformStack {
         vmSize: "Standard_B2s",
       },
     });
-
-    new KubernetesClusterNodePool(this, "spot-node-pool", {
-      name: "spot",
-      kubernetesClusterId: k8sCluster.id,
-      vmSize: "Standard_B2s",
-      nodeCount: 1,
-      minCount: 1,
-      maxCount: 3,
-      priority: "Spot",
-      evictionPolicy: "Delete",
-      enableAutoScaling: true,
-      nodeLabels: {
-        "kubernetes.azure.com/scalesetpriority": "spot",
-      },
-      nodeTaints: ["kubernetes.azure.com/scalesetpriority=spot:NoSchedule"],
-    });
-
-    // TODO: Configure flux automatically here when this PR is merged:
-    // https://github.com/hashicorp/terraform-provider-azurerm/issues/15011
   }
 }
 
