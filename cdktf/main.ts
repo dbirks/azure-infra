bf869eefee702b70ce3e956a8f5b6b289b32591c
David Birks <david@birks.dev>
2023-05-18 23:13:55 -0400

    Switch back to a publicly exposed cluster for $$ savings

--- cdktf/main.ts
+++ cdktf/main.ts
@@ -3,8 +3,6 @@ import { App, TerraformStack } from "cdktf";
 import { kubernetesCluster } from "@cdktf/provider-azurerm";
 import { AzurermProvider } from "@cdktf/provider-azurerm/lib/provider";
 import { VirtualNetwork } from "@cdktf/provider-azurerm/lib/virtual-network";
-import { PrivateDnsZone } from "@cdktf/provider-azurerm/lib/private-dns-zone";
-import { PrivateDnsResolver } from "@cdktf/provider-azurerm/lib/private-dns-resolver";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
@@ -15,21 +13,7 @@ class MyStack extends TerraformStack {
 
     new AzurermProvider(this, "azurerm", { features: {} });
 
-    // const wan = new VirtualWan(this, "wan", {
-    //   name: "birks-wan",
-    //   resourceGroupName,
-    //   location,
-    // });
-
-    // new VirtualHub(this, "hub", {
-    //   name: "birks-hub",
-    //   resourceGroupName,
-    //   location,
-    //   virtualWanId: wan.id,
-    //   addressPrefix: "10.10.0.0/20",
-    // });
-
-    const vnet = new VirtualNetwork(this, "vnet", {
+    new VirtualNetwork(this, "vnet", {
       name: "personal",
       location,
       resourceGroupName,
@@ -39,28 +23,8 @@ class MyStack extends TerraformStack {
           name: "default",
           addressPrefix: "10.10.0.0/23",
         },
-        {
-          name: "GatewaySubnet",
-          addressPrefix: "10.10.2.0/24",
-        },
-        {
-          name: "dns-resolvers",
-          addressPrefix: "10.10.3.0/28",
-        },
       ],
-      // gatewaySubnet: [
-      //   {
-      //     name: "gateway",
-      //     addressPrefix: "10.10.2.0/24",
-      //   }
     });
-
-    // new VpnGateway(this, "vpn", {
-    //   name: "Home connection",
-    //   resourceGroupName,
-    //   location,
-    //   virtualHubId: hub.id,
-    // });
 
     // TODO: Re-enable this when my subscription can create spot instances
     // new KubernetesClusterNodePool(this, "spot-node-pool", {
@@ -82,30 +46,9 @@ class MyStack extends TerraformStack {
     // TODO: Configure flux automatically here when this PR is merged:
     // https://github.com/hashicorp/terraform-provider-azurerm/issues/15011
 
-    new PrivateDnsZone(this, "private-dns", {
-      name: "birks.local",
-      resourceGroupName,
-    });
-
-    new PrivateDnsResolver(this, "private-dns-resolver", {
-      name: "private-resolver",
-      resourceGroupName,
-      location,
-      virtualNetworkId: vnet.id,
-      // privateDnsZoneConfigurations: [
-      //   {
-      //     name: "default",
-      //     privateDnsZoneId: "birks.local",
-      //     registrationEnabled: true,
-      //     resolutionEnabled: true,
-      //   },
-      // ],
-    });
-
     new kubernetesCluster.KubernetesCluster(this, "k8s", {
       name: "home",
       dnsPrefix: "home", // prefix of the hostname of the publicly exposed cluster api
-      privateClusterEnabled: true,
       // privateDnsZoneId: privateDns.id,
       location: "eastus",
       resourceGroupName,
